0823b3d87aec1048bb0eaad61304919d56557179
ptenteromano <philip.tenteromano@gmail.com>
2018-04-06 19:14:43 +0000

    add user data to articles listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    @article.user = User.first              # temporary 
+    @article.user = User.first              # temporary !!
     if @article.save
       # flast shows on first redirect
       flash[:success] = "Article was successfully created"
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article<ActiveRecord::Base
   # enforcing some constrants on article creation
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
   validates :description, presence: true, length: {minimum: 10, maximum: 300}
+  validates :user_id, presence: true
 end
