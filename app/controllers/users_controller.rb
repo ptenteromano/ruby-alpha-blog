fd9890fa66f724e65d3a039a86f47a9907cf6918
ptenteromano <philip.tenteromano@gmail.com>
2018-04-09 22:07:10 +0000

    add style and HTTPS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :require_admin, only: [:destroy]
   
   def index
-    @users = User.paginate(page: params[:page], per_page: 5)
+    @users = User.paginate(page: params[:page], per_page: 8)
   end
 
   def new
